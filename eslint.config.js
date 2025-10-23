85ec30542a4904b6551afa712ef05a5583865bb3
Nipun Jindal <mansapatiala@gmail.com>
2025-10-24 01:05:33 +0530

    eslint and prettier config

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+    {
+        languageOptions: { globals: { ...globals.browser, ...globals.node } }
+    },
+    pluginJs.configs.recommended,
+    {
+        // Ignore files and directories
+        ignores: ["**/*.sol", "lib/", "out/", "cache/"]
+    }
+];
